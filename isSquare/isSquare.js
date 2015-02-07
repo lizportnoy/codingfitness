474c841ee3a3176fad1ac8d0e37980af361b6a8f
Liz Portnoy <lizport10@gmail.com>
2015-02-06 19:26:40 -0500

    isSquare

--- isSquare/isSquare.js
+++ isSquare/isSquare.js
@@ -2,6 +2,15 @@
 // whether the integer n is the square of another integer number.
 // Do not use Math.pow(n, 0.5) in your solution.
 
-var isSquare = function(n){
+var isSquare = function(n, multiplier){
   // Your code here
-}
+  var multiplier = multiplier || 1;
+  var square = multiplier * multiplier;
+  if (square === n) {
+    return true;
+  } else if (square > n) {
+    return false;
+  } else {
+    return isSquare(n, multiplier + 1);
+  }
+};
